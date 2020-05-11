447ba176b6f12f23d51a185788e34918d7b90200
Anders Åstrand <aastrand@gmail.com>
2020-05-11 22:59:44 +0200

    Move IO to separate module

---
+++ src/emu/io.rs
@@ -0,0 +1,99 @@
+use super::memory;
+use pancurses;
+use std::{thread, time};
+
+pub trait IOHandler {
+    fn init(&mut self);
+    fn log(&self, logline: &str);
+    fn display(&self, mem: &memory::Memory);
+    fn input(&mut self, mem: &mut memory::Memory);
+    fn exit(&self, s: &str);
+}
+
+pub struct HeadlessIOHandler {}
+
+impl IOHandler for HeadlessIOHandler {
+    fn init(&mut self) {}
+
+    fn log(&self, logline: &str) {
+        println!("{}", logline);
+    }
+
+    #[allow(unused_variables)]
+    fn input(&mut self, mem: &mut memory::Memory) {
+        // TOOO
+    }
+
+    #[allow(unused_variables)]
+    fn display(&self, mem: &memory::Memory) {}
+
+    fn exit(&self, s: &str) {
+        println!("{}", s);
+    }
+}
+
+pub struct CursesIOHandler {
+    window: pancurses::Window,
+}
+
+impl CursesIOHandler {
+    pub fn new() -> CursesIOHandler {
+        CursesIOHandler {
+            window: pancurses::initscr(),
+        }
+    }
+}
+
+impl IOHandler for CursesIOHandler {
+    fn init(&mut self) {
+        self.window.timeout(0);
+    }
+
+    fn log(&self, logline: &str) {
+        self.window.mvaddstr(0, 0, logline);
+    }
+
+    fn input(&mut self, mem: &mut memory::Memory) {
+        // TODO: Map more input
+        match self.window.getch() {
+            Some(pancurses::Input::Character(c)) => {
+                mem.ram[0xff] = c.to_ascii_lowercase() as u8;
+            }
+            _ => {}
+        }
+        self.window.refresh();
+    }
+
+    fn display(&self, mem: &memory::Memory) {
+        // Display is stored as a 32x32 screen from 0x200 and onwards
+        let base: u16 = 0x0200;
+        let offset: i32 = 3;
+        self.window.attron(pancurses::A_REVERSE);
+
+        // Apple location is stored at 0x0100
+        let apple_addr: u16 = mem.get_16b_addr(0x00);
+        for y in 0..31 {
+            for x in 0..31 {
+                let addr: u16 = base + (y * 32) + x;
+                let chr: char = if addr == apple_addr {
+                    'O'
+                } else {
+                    let value: u8 = mem.ram[addr as usize];
+                    if value == 1 {
+                        '#'
+                    } else {
+                        ' '
+                    }
+                };
+                self.window.mvaddch(offset + y as i32, x as i32, chr);
+            }
+        }
+        self.window.attroff(pancurses::A_REVERSE);
+        thread::sleep(time::Duration::from_micros(10));
+    }
+
+    fn exit(&self, s: &str) {
+        self.window.mvaddstr(1, 0, s);
+        pancurses::endwin();
+    }
+}
--- src/emu/mod.rs
+++ src/emu/mod.rs
@@ -1,112 +1,15 @@
 pub mod cpu;
+pub mod io;
 pub mod memory;
 pub mod opcodes;
 
-use pancurses;
 use rand::Rng;
-use std::{thread, time};
-
-trait IOHandler {
-    fn init(&mut self);
-    fn log(&self, logline: &str);
-    fn display(&self, mem: &memory::Memory);
-    fn input(&mut self, mem: &mut memory::Memory);
-    fn exit(&self, s: &str);
-}
-
-pub struct HeadlessIOHandler {}
-
-impl IOHandler for HeadlessIOHandler {
-    fn init(&mut self) {}
-
-    fn log(&self, logline: &str) {
-        println!("{}", logline);
-    }
-
-    #[allow(unused_variables)]
-    fn input(&mut self, mem: &mut memory::Memory) {
-        // TOOO
-    }
-
-    #[allow(unused_variables)]
-    fn display(&self, mem: &memory::Memory) {}
-
-    fn exit(&self, s: &str) {
-        println!("{}", s);
-    }
-}
-
-pub struct CursesIOHandler {
-    window: pancurses::Window,
-}
-
-impl CursesIOHandler {
-    fn new() -> CursesIOHandler {
-        CursesIOHandler {
-            window: pancurses::initscr(),
-        }
-    }
-}
-
-impl IOHandler for CursesIOHandler {
-    fn init(&mut self) {
-        self.window.timeout(0);
-    }
-
-    fn log(&self, logline: &str) {
-        self.window.mvaddstr(0, 0, logline);
-    }
-
-    fn input(&mut self, mem: &mut memory::Memory) {
-        // TODO: Map more input
-        match self.window.getch() {
-            Some(pancurses::Input::Character(c)) => {
-                mem.ram[0xff] = c.to_ascii_lowercase() as u8;
-            }
-            _ => {}
-        }
-        self.window.refresh();
-    }
-
-    fn display(&self, mem: &memory::Memory) {
-        // Display is stored as a 32x32 screen from 0x200 and onwards
-        let base: u16 = 0x0200;
-        let offset: i32 = 3;
-        self.window.attron(pancurses::A_REVERSE);
-
-        // Apple location is stored at 0x0100
-        let apple_addr: u16 = mem.get_16b_addr(0x00);
-        for y in 0..31 {
-            for x in 0..31 {
-                let addr: u16 = base + (y * 32) + x;
-                let chr: char = if addr == apple_addr {
-                    'O'
-                } else {
-                    let value: u8 = mem.ram[addr as usize];
-                    if value == 1 {
-                        '#'
-                    } else {
-                        ' '
-                    }
-                };
-                self.window.mvaddch(offset + y as i32, x as i32, chr);
-            }
-        }
-        self.window.attroff(pancurses::A_REVERSE);
-        thread::sleep(time::Duration::from_micros(10));
-    }
-
-    fn exit(&self, s: &str) {
-        self.window.mvaddstr(1, 0, s);
-        pancurses::endwin();
-    }
-}
 
 pub struct Emulator {
     pub cpu: cpu::Cpu,
     pub mem: memory::Memory,
     lookup: opcodes::Lookup,
-    iohandler: Box<dyn IOHandler>,
+    iohandler: Box<dyn io::IOHandler>,
     rng: rand::rngs::ThreadRng,
 }
 
@@ -116,7 +19,7 @@ impl Emulator {
             cpu: cpu::Cpu::new(),
             mem: memory::Memory::new(),
             lookup: opcodes::Lookup::new(),
-            iohandler: Box::new(CursesIOHandler::new()),
+            iohandler: Box::new(io::CursesIOHandler::new()),
             rng: rand::thread_rng(),
         }
     }
@@ -127,7 +30,7 @@ impl Emulator {
             cpu: cpu::Cpu::new(),
             mem: memory::Memory::new(),
             lookup: opcodes::Lookup::new(),
-            iohandler: Box::new(HeadlessIOHandler {}),
+            iohandler: Box::new(io::HeadlessIOHandler {}),
             rng: rand::thread_rng(),
         }
     }
