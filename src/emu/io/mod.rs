807fa60836fd687b1ee2106746a9b038754dd357
Anders Åstrand <anders@teknodromen.se>
2026-05-04 20:15:45 +0200

    Migrate winit to pump_events/ApplicationHandler API
    Replace deprecated EventLoopExtRunReturn with EventLoopExtPumpEvents
    and move event handling into ApplicationHandler implementations.

--- src/emu/io/mod.rs
+++ src/emu/io/mod.rs
@@ -2,13 +2,17 @@ pub mod controller;
 pub mod loader;
 pub mod log;
 
+use std::time::Duration;
+
 use pixels::{Pixels, SurfaceTexture};
-use winit::platform::run_return::EventLoopExtRunReturn;
+use winit::platform::pump_events::EventLoopExtPumpEvents;
 use winit::{
+    application::ApplicationHandler,
     dpi::LogicalSize,
-    event::{ElementState, Event, VirtualKeyCode, WindowEvent},
-    event_loop::{ControlFlow, EventLoop},
-    window::WindowBuilder,
+    event::{ElementState, WindowEvent},
+    event_loop::{ActiveEventLoop, EventLoop},
+    keyboard::{KeyCode, PhysicalKey},
+    window::{Window, WindowAttributes},
 };
 
 use super::cpu;
@@ -47,33 +51,197 @@ impl IOHandler for HeadlessIOHandler {
 }
 
 pub struct WinitPixelsIOHandler {
-    pub pixels: Pixels,
-    pub event_loop: Option<EventLoop<()>>,
-    pub window: winit::window::Window,
-    pub muted: bool,
-    // (deprecated) APU $4015 masking removed; using APU override instead
+    pixels: Option<Pixels<'static>>,
+    event_loop: Option<EventLoop<()>>,
+    window: Option<&'static Window>,
+    muted: bool,
+}
+
+struct InitHandler {
+    window: Option<&'static Window>,
+    pixels: Option<Pixels<'static>>,
+    width: u32,
+    height: u32,
+}
+
+impl ApplicationHandler for InitHandler {
+    fn resumed(&mut self, event_loop: &ActiveEventLoop) {
+        let scale = 4.0;
+        let window_width = (self.width as f32 * scale) as u32;
+        let window_height = (self.height as f32 * scale) as u32;
+        let attrs = WindowAttributes::default()
+            .with_title("krankulator")
+            .with_inner_size(LogicalSize::new(window_width, window_height));
+        let window = event_loop.create_window(attrs).unwrap();
+        let window: &'static Window = Box::leak(Box::new(window));
+        let surface_texture = SurfaceTexture::new(self.width, self.height, window);
+        let pixels = Pixels::new(self.width, self.height, surface_texture).unwrap();
+        self.window = Some(window);
+        self.pixels = Some(pixels);
+    }
+
+    fn window_event(&mut self, _event_loop: &ActiveEventLoop, _id: winit::window::WindowId, _event: WindowEvent) {}
 }
 
 impl WinitPixelsIOHandler {
     pub fn new(width: u32, height: u32) -> Self {
-        let scale = 4.0;
-        let window_width = (width as f32 * scale) as u32;
-        let window_height = (height as f32 * scale) as u32;
-        let event_loop = EventLoop::new();
-        let window = WindowBuilder::new()
-            .with_title("krankulator")
-            .with_inner_size(LogicalSize::new(window_width, window_height))
-            .build(&event_loop)
-            .unwrap();
+        let mut event_loop = EventLoop::new().unwrap();
 
-        let surface_texture = SurfaceTexture::new(width, height, &window);
-        let pixels = Pixels::new(width, height, surface_texture).unwrap();
+        let mut init = InitHandler {
+            window: None,
+            pixels: None,
+            width,
+            height,
+        };
+
+        loop {
+            event_loop.pump_app_events(Some(Duration::ZERO), &mut init);
+            if init.window.is_some() {
+                break;
+            }
+        }
 
         Self {
+            pixels: init.pixels,
             event_loop: Some(event_loop),
-            window,
-            pixels,
+            window: init.window,
             muted: false,
+        }
+    }
+}
+
+struct PollHandler<'a> {
+    pixels: &'a mut Pixels<'static>,
+    mem: &'a mut dyn memory::MemoryMapper,
+    cpu: &'a mut cpu::Cpu,
+    muted: &'a mut bool,
+    exit: bool,
+}
+
+impl ApplicationHandler for PollHandler<'_> {
+    fn resumed(&mut self, _event_loop: &ActiveEventLoop) {}
+
+    fn window_event(&mut self, event_loop: &ActiveEventLoop, _id: winit::window::WindowId, event: WindowEvent) {
+        match event {
+            WindowEvent::Resized(size) => {
+                let _ = self.pixels.resize_surface(size.width, size.height);
+            }
+            WindowEvent::CloseRequested => {
+                self.exit = true;
+                event_loop.exit();
+            }
+            WindowEvent::KeyboardInput { event, .. } => {
+                if let PhysicalKey::Code(key) = event.physical_key {
+                    let pressed = event.state == ElementState::Pressed;
+
+                    match key {
+                        KeyCode::Escape => {
+                            self.exit = true;
+                            event_loop.exit();
+                        }
+                        KeyCode::KeyM => {
+                            if pressed {
+                                *self.muted ^= true;
+                            }
+                        }
+                        KeyCode::KeyR => {
+                            if pressed {
+                                self.cpu.pc = self.mem.get_16b_addr(memory::RESET_TARGET_ADDR);
+                                self.mem.apu().borrow_mut().reset();
+                            }
+                        }
+                        KeyCode::Digit1 => {
+                            if pressed {
+                                self.mem.apu().borrow_mut().toggle_mute_bit(0x01, "Pulse1");
+                            }
+                        }
+                        KeyCode::Digit2 => {
+                            if pressed {
+                                self.mem.apu().borrow_mut().toggle_mute_bit(0x02, "Pulse2");
+                            }
+                        }
+                        KeyCode::Digit3 => {
+                            if pressed {
+                                self.mem.apu().borrow_mut().toggle_mute_bit(0x04, "Triangle");
+                            }
+                        }
+                        KeyCode::Digit4 => {
+                            if pressed {
+                                self.mem.apu().borrow_mut().toggle_mute_bit(0x08, "Noise");
+                            }
+                        }
+                        KeyCode::Digit5 => {
+                            if pressed {
+                                self.mem.apu().borrow_mut().toggle_mute_bit(0x10, "DMC");
+                            }
+                        }
+                        KeyCode::Digit0 => {
+                            if pressed {
+                                let on = !self.mem.apu().borrow().get_master_mute();
+                                self.mem.apu().borrow_mut().set_master_mute(on);
+                            }
+                        }
+                        KeyCode::KeyZ => {
+                            if pressed {
+                                self.mem.controllers()[0].set_pressed(controller::A);
+                            } else {
+                                self.mem.controllers()[0].set_not_pressed(controller::A);
+                            }
+                        }
+                        KeyCode::KeyX => {
+                            if pressed {
+                                self.mem.controllers()[0].set_pressed(controller::B);
+                            } else {
+                                self.mem.controllers()[0].set_not_pressed(controller::B);
+                            }
+                        }
+                        KeyCode::KeyC => {
+                            if pressed {
+                                self.mem.controllers()[0].set_pressed(controller::START);
+                            } else {
+                                self.mem.controllers()[0].set_not_pressed(controller::START);
+                            }
+                        }
+                        KeyCode::KeyV => {
+                            if pressed {
+                                self.mem.controllers()[0].set_pressed(controller::SELECT);
+                            } else {
+                                self.mem.controllers()[0].set_not_pressed(controller::SELECT);
+                            }
+                        }
+                        KeyCode::ArrowLeft => {
+                            if pressed {
+                                self.mem.controllers()[0].set_pressed(controller::LEFT);
+                            } else {
+                                self.mem.controllers()[0].set_not_pressed(controller::LEFT);
+                            }
+                        }
+                        KeyCode::ArrowRight => {
+                            if pressed {
+                                self.mem.controllers()[0].set_pressed(controller::RIGHT);
+                            } else {
+                                self.mem.controllers()[0].set_not_pressed(controller::RIGHT);
+                            }
+                        }
+                        KeyCode::ArrowUp => {
+                            if pressed {
+                                self.mem.controllers()[0].set_pressed(controller::UP);
+                            } else {
+                                self.mem.controllers()[0].set_not_pressed(controller::UP);
+                            }
+                        }
+                        KeyCode::ArrowDown => {
+                            if pressed {
+                                self.mem.controllers()[0].set_pressed(controller::DOWN);
+                            } else {
+                                self.mem.controllers()[0].set_not_pressed(controller::DOWN);
+                            }
+                        }
+                        _ => {}
+                    }
+                }
+            }
+            _ => {}
         }
     }
 }
@@ -90,156 +258,26 @@ impl IOHandler for WinitPixelsIOHandler {
     }
 
     fn poll(&mut self, mem: &mut dyn memory::MemoryMapper, cpu: &mut cpu::Cpu) -> bool {
-        let mut exit = false;
-
         let mut event_loop = self.event_loop.take().unwrap();
 
-        event_loop.run_return(|event, _, control_flow| {
-            *control_flow = ControlFlow::Poll;
+        let mut handler = PollHandler {
+            pixels: self.pixels.as_mut().unwrap(),
+            mem,
+            cpu,
+            muted: &mut self.muted,
+            exit: false,
+        };
 
-            match event {
-                Event::MainEventsCleared => {
-                    *control_flow = ControlFlow::Exit;
-                }
-                Event::WindowEvent { event, .. } => match event {
-                    WindowEvent::Resized(size) => {
-                        let _ = self.pixels.resize_surface(size.width, size.height);
-                    }
-                    WindowEvent::ScaleFactorChanged { new_inner_size, .. } => {
-                        let _ = self
-                            .pixels
-                            .resize_surface(new_inner_size.width, new_inner_size.height);
-                    }
-                    WindowEvent::CloseRequested => {
-                        exit = true;
-                        *control_flow = ControlFlow::Exit;
-                    }
-                    WindowEvent::KeyboardInput { input, .. } => {
-                        if let Some(key) = input.virtual_keycode {
-                            let pressed = input.state == ElementState::Pressed;
-
-                            match key {
-                                VirtualKeyCode::Escape => {
-                                    exit = true;
-                                    *control_flow = ControlFlow::Exit;
-                                }
-                                VirtualKeyCode::M => {
-                                    if pressed {
-                                        self.muted ^= true;
-                                    }
-                                }
-                                VirtualKeyCode::R => {
-                                    if pressed {
-                                        cpu.pc = mem.get_16b_addr(memory::RESET_TARGET_ADDR);
-                                        mem.apu().borrow_mut().reset();
-                                    }
-                                }
-                                // APU channel mute toggles for quick isolation (override mute)
-                                // 1..5 toggle Pulse1, Pulse2, Triangle, Noise, DMC respectively
-                                VirtualKeyCode::Key1 => {
-                                    if pressed {
-                                        mem.apu().borrow_mut().toggle_mute_bit(0x01, "Pulse1");
-                                    }
-                                }
-                                VirtualKeyCode::Key2 => {
-                                    if pressed {
-                                        mem.apu().borrow_mut().toggle_mute_bit(0x02, "Pulse2");
-                                    }
-                                }
-                                VirtualKeyCode::Key3 => {
-                                    if pressed {
-                                        mem.apu().borrow_mut().toggle_mute_bit(0x04, "Triangle");
-                                    }
-                                }
-                                VirtualKeyCode::Key4 => {
-                                    if pressed {
-                                        mem.apu().borrow_mut().toggle_mute_bit(0x08, "Noise");
-                                    }
-                                }
-                                VirtualKeyCode::Key5 => {
-                                    if pressed {
-                                        mem.apu().borrow_mut().toggle_mute_bit(0x10, "DMC");
-                                    }
-                                }
-                                // 0 toggles master mute (all channels)
-                                VirtualKeyCode::Key0 => {
-                                    if pressed {
-                                        let on = !mem.apu().borrow().get_master_mute();
-                                        mem.apu().borrow_mut().set_master_mute(on);
-                                    }
-                                }
-                                VirtualKeyCode::Z => {
-                                    if pressed {
-                                        mem.controllers()[0].set_pressed(controller::A);
-                                    } else {
-                                        mem.controllers()[0].set_not_pressed(controller::A);
-                                    }
-                                }
-                                VirtualKeyCode::X => {
-                                    if pressed {
-                                        mem.controllers()[0].set_pressed(controller::B);
-                                    } else {
-                                        mem.controllers()[0].set_not_pressed(controller::B);
-                                    }
-                                }
-                                VirtualKeyCode::C => {
-                                    if pressed {
-                                        mem.controllers()[0].set_pressed(controller::START);
-                                    } else {
-                                        mem.controllers()[0].set_not_pressed(controller::START);
-                                    }
-                                }
-                                VirtualKeyCode::V => {
-                                    if pressed {
-                                        mem.controllers()[0].set_pressed(controller::SELECT);
-                                    } else {
-                                        mem.controllers()[0].set_not_pressed(controller::SELECT);
-                                    }
-                                }
-                                VirtualKeyCode::Left => {
-                                    if pressed {
-                                        mem.controllers()[0].set_pressed(controller::LEFT);
-                                    } else {
-                                        mem.controllers()[0].set_not_pressed(controller::LEFT);
-                                    }
-                                }
-                                VirtualKeyCode::Right => {
-                                    if pressed {
-                                        mem.controllers()[0].set_pressed(controller::RIGHT);
-                                    } else {
-                                        mem.controllers()[0].set_not_pressed(controller::RIGHT);
-                                    }
-                                }
-                                VirtualKeyCode::Up => {
-                                    if pressed {
-                                        mem.controllers()[0].set_pressed(controller::UP);
-                                    } else {
-                                        mem.controllers()[0].set_not_pressed(controller::UP);
-                                    }
-                                }
-                                VirtualKeyCode::Down => {
-                                    if pressed {
-                                        mem.controllers()[0].set_pressed(controller::DOWN);
-                                    } else {
-                                        mem.controllers()[0].set_not_pressed(controller::DOWN);
-                                    }
-                                }
-                                _ => {}
-                            }
-                        }
-                    }
-                    _ => {}
-                },
-                _ => {}
-            }
-        });
+        event_loop.pump_app_events(Some(Duration::ZERO), &mut handler);
+        let exit = handler.exit;
 
         self.event_loop = Some(event_loop);
         exit
     }
 
     fn render(&mut self, buf: &gfx::buf::Buffer) {
-        let frame = self.pixels.frame_mut();
+        let pixels = self.pixels.as_mut().unwrap();
+        let frame = pixels.frame_mut();
         let pixel_count = buf.data.len() / 3;
         for i in 0..pixel_count {
             let rgb = &buf.data[i * 3..i * 3 + 3];
@@ -248,11 +286,11 @@ impl IOHandler for WinitPixelsIOHandler {
                 frame[j] = rgb[0];
                 frame[j + 1] = rgb[1];
                 frame[j + 2] = rgb[2];
-                frame[j + 3] = 255; // Opaque alpha
+                frame[j + 3] = 255;
             }
         }
-        self.pixels.render().unwrap();
-        self.window.request_redraw();
+        pixels.render().unwrap();
+        self.window.unwrap().request_redraw();
     }
 
     fn exit(&self, s: String) {
